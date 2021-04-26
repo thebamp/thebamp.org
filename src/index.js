abf341b678affcb18f3585ec0647d36e08df3c7d
John Cipriano <johnmikecip@gmail.com>
2021-04-26 00:44:06 -0700

    replace javascript with ours

--- src/index.js
+++ src/index.js
@@ -1,6 +1,86 @@
-// JS Goes here - ES6 supported
+import './css/main.css'
 
-import "./css/main.css";
+import $ from 'jquery'
 
-// Say hello
-console.log("🦊 Hello! Edit me in src/index.js");
+import './instaFeed.js'
+import './carousel.js'
+
+/*
+ * restore animations once dom has settled
+ */
+$(document).ready(() => {
+    $('body').removeClass("u-preload")
+})
+
+
+/*
+ * show/hide nav modal
+ */
+$('#hamburger').add('#closeNavModal').add('.navModal--linkList > *')
+    .click(() => {
+        $('#navModal').toggleClass('navModal-is-shown')
+        $('body').toggleClass('l-noScroll')
+    })
+
+
+/*
+ * show/hide faq answers
+ */
+$('.faq--questionBox').click(event => {
+    $(event.target).closest('.faq').toggleClass('faq-is-open')
+    event.preventDefault()
+})
+
+
+/*
+ * jump to email form from "get updates"
+ */
+$('.getUpdates').click(() => {
+    $('.emailForm--inputBox').focus()
+})
+
+
+/*
+ * show/hide mural modal
+ */
+function showMuralModal(event) {
+    const name = event.target.closest('.muralThumbnail[data-mural-name]').dataset.muralName
+    $(`.muralModalContainer[data-mural-name="${name}"]`).addClass('muralModalContainer-is-shown')
+    $('body').addClass('l-noScroll')
+}
+
+function hideMuralModal() {
+    $('.muralModalContainer-is-shown').removeClass('muralModalContainer-is-shown')
+    $('body').removeClass('l-noScroll')
+}
+
+$('.muralThumbnail').click(showMuralModal)
+$('.muralModal--close').click(hideMuralModal)
+$('.muralModalContainer').click(hideMuralModal).children().click(() => { return false })
+$(document).on('keyup', e => { e.key === "Escape" ? hideMuralModal() : null })
+
+
+/*
+ * mural gallery
+ */
+$('#muralGallerySlider').click(event => {
+    $('#muralGallery').toggleClass('muralGallery-is-hidden')
+    if (event.target.innerHTML === "&#10095;") {
+        event.target.innerHTML = "&#10094;"
+    } else {
+        event.target.innerHTML = "&#10095;"
+    }
+})
+
+
+/*
+ * make mobile carousel copies of gridbox (programs & events)
+ */
+$('.js-photoGrid').each((idx, pg) => {
+    const pics = $(pg).find('img').clone().removeClass('fixedRatio--contents').addClass('carousel--item carousel--item-cover')
+    const section = $(pg).parent()
+    const wrapper = $('<div>').addClass('u-onlyMobile')
+    wrapper.append(pics)
+    section.append(wrapper)
+    wrapper.addClass('js-carousel-standard slick-slider slick-dotted')
+})
---
+++ src/instaFeed.js
@@ -0,0 +1,48 @@
+import $ from 'jquery'
+
+/**
+ * Renders Instagram feed JSON response in the given DOM target.
+ */
+function renderFeed(media, target, length) {
+    const grid = $('<div>').addClass('gridBox gridBox-col4 gridBox-center')
+
+    for (let i = 0; i < length && i < media.length; i++) {
+        const node = media[i].node
+        const type = node.__typename
+        const post = $('<div>').addClass('fixedRatio')
+        let contents = null
+
+        if (type === 'GraphImage' || type === 'GraphSidecar') {
+            contents = $('<img>').attr('src', node.display_url)
+        } else if (type === 'GraphVideo') {
+            const source = $('<source>').attr('src', node.video_url)
+            contents = $('<video>').attr('controls', true).append(source)
+        } else {
+            continue
+        }
+        post.append(contents.addClass('fixedRatio--contents'))
+        grid.append(post)
+    }
+    $(target).empty().append(grid)
+}
+
+/**
+ * Fetches IG account feed JSON and renders (if valid response) into the given target.
+ */
+async function fetchAndRenderFeed(account, target, length) {
+    try {
+        const response = await fetch(`https://www.instagram.com/${account}/?__a=1`).then(x => x.json())
+        const media = response.graphql.user.edge_owner_to_timeline_media.edges
+        renderFeed(media, target, length)
+    } catch (error) {
+        console.log(`Error: could not load IG profile for {accountName}.`)
+        console.log(error)
+    }
+}
+
+// hide behind a button during development (TODO: remove for production)
+$('.js-showInstaFeed').click(event => {
+    fetchAndRenderFeed('bayareamuralpro', $('.instagramFeed')[0], 8)
+    event.target.disabled = true
+    event.target.style.display = 'none'
+})
