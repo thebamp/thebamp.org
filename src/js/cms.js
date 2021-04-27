9c1769a291e44b1cd749522138fc377b6bb22616
John Cipriano <johnmikecip@gmail.com>
2021-04-26 19:31:08 -0700

    remove products mention from cms.js

--- src/js/cms.js
+++ src/js/cms.js
@@ -12,7 +12,6 @@ import ContactPreview from "./cms-preview-templates/contact";
 CMS.registerPreviewStyle(styles, { raw: true });
 CMS.registerPreviewTemplate("home", HomePreview);
 CMS.registerPreviewTemplate("post", PostPreview);
-CMS.registerPreviewTemplate("products", ProductsPreview);
 CMS.registerPreviewTemplate("values", ValuesPreview);
 CMS.registerPreviewTemplate("contact", ContactPreview);
 CMS.init();
