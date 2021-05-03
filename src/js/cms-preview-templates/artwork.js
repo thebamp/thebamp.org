8b7cac6d1bc450688c915c62f471aab4ff4c74b6
John Cipriano <johnmikecip@gmail.com>
2021-05-02 21:14:29 -0700

    post-artwork JS tweakz

--- src/js/cms-preview-templates/artwork.js
+++ src/js/cms-preview-templates/artwork.js
@@ -10,47 +10,45 @@ export default ({entry, getAsset}) => {
     const faqs = entry.getIn(["data", "faqs"]) || []
     const murals = entry.getIn(["data", "murals"]) || []
 
-    return (
-        <React.Fragment>
-            <section className="l-section">
-                <div className="gridBox gridBox-col2 gridBox-col2-collapse">
-                    <WeSeekTo/>
-                    <MapBox/>
-                </div>
-            </section>
-            <section className="l-section">
-                <div className="gridBox gridBox-col3 gridBox-center">
-                    {murals.map((mural, i) => {
-                        return <button className={`muralThumbnail box box-${mural.get("boxColor")}`} key={i}>
-                            <div className="muralThumbnail--img"><img src={getAsset(mural.get("thumbUrl"))}/></div>
-                            <div className="muralThumbnail--caption">
-                                <div className="muralThumbnail--location">{mural.get("location")}</div>
-                                <div className="muralThumbnail--name">{mural.get("title")}</div>
-                            </div>
-                        </button>
-                    })}
-                </div>
-            </section>
-            <section className="l-section">
-                <div className="gridBox gridBox-col2 gridBox-col2-collapse">
-                    <Youtube src="https://www.youtube.com/embed/OaKg3WeMKrI"/>
-                    <WantAMural/>
-                </div>
-            </section>
-            <section className="l-section">
-                <h2 className="sectionHeading sectionHeading-centered sectionHeading-spaced">Frequently Asked Questions</h2>
-                {faqs.map((faq, i) =>
-                    <div className="faq faq-is-open" key={i}>
-                        <button className="faq--questionBox">
-                            <div className="faq--question">{faq.get("question")}</div>
-                            <div className="faq--plus">+</div>
-                        </button>
-                        <div className="faq--answer">
-                            <ReactMarkdown>{faq.get("answer")}</ReactMarkdown>
+    return <React.Fragment>
+        <section className="l-section">
+            <div className="gridBox gridBox-col2 gridBox-col2-collapse">
+                <WeSeekTo/>
+                <MapBox/>
+            </div>
+        </section>
+        <section className="l-section">
+            <div className="gridBox gridBox-col3 gridBox-center">
+                {murals.map((mural, i) => {
+                    return <button className={`muralThumbnail box box-${mural.get("boxColor")}`} key={i}>
+                        <div className="muralThumbnail--img"><img src={getAsset(mural.get("thumbUrl"))}/></div>
+                        <div className="muralThumbnail--caption">
+                            <div className="muralThumbnail--location">{mural.get("location")}</div>
+                            <div className="muralThumbnail--name">{mural.get("title")}</div>
                         </div>
+                    </button>
+                })}
+            </div>
+        </section>
+        <section className="l-section">
+            <div className="gridBox gridBox-col2 gridBox-col2-collapse">
+                <Youtube src="https://www.youtube.com/embed/OaKg3WeMKrI"/>
+                <WantAMural/>
+            </div>
+        </section>
+        <section className="l-section">
+            <h2 className="sectionHeading sectionHeading-centered sectionHeading-spaced">Frequently Asked Questions</h2>
+            {faqs.map((faq, i) =>
+                <div className="faq faq-is-open" key={i}>
+                    <button className="faq--questionBox">
+                        <div className="faq--question">{faq.get("question")}</div>
+                        <div className="faq--plus">+</div>
+                    </button>
+                    <div className="faq--answer">
+                        <ReactMarkdown>{faq.get("answer")}</ReactMarkdown>
                     </div>
-                )}
-            </section>
-        </React.Fragment>
-    )
+                </div>
+            )}
+        </section>
+    </React.Fragment>
 }
--- src/js/cms.js
+++ src/js/cms.js
@@ -4,6 +4,8 @@ import CMS from "netlify-cms-app";
 // Import main site styles as a string to inject into the CMS preview pane
 import styles from "!to-string-loader!css-loader!postcss-loader!sass-loader!../css/main.css";
 
+// ----- demo stuff ------
+
 import HomePreview from "./cms-preview-templates/home";
 import PostPreview from "./cms-preview-templates/post";
 import ValuesPreview from "./cms-preview-templates/values";
@@ -14,6 +16,8 @@ CMS.registerPreviewTemplate("home", HomePreview);
 CMS.registerPreviewTemplate("post", PostPreview);
 CMS.registerPreviewTemplate("values", ValuesPreview);
 CMS.registerPreviewTemplate("contact", ContactPreview);
+
+// ----- end demo stuff -----
 
 import ArtworkPreview from "./cms-preview-templates/artwork";
 CMS.registerPreviewTemplate("artwork", ArtworkPreview);
