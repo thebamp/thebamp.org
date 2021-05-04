a17b4be5a8af6669c6f3aa07052e258b5302585e
John Cipriano <johnmikecip@gmail.com>
2021-05-03 18:17:58 -0700

    remove demo files & update readme

--- src/js/cms.js
+++ src/js/cms.js
@@ -4,21 +4,6 @@ import CMS from "netlify-cms-app";
 // Import main site styles as a string to inject into the CMS preview pane
 import styles from "!to-string-loader!css-loader!postcss-loader!sass-loader!../css/main.css";
 
-// ----- demo stuff ------
-
-import HomePreview from "./cms-preview-templates/home";
-import PostPreview from "./cms-preview-templates/post";
-import ValuesPreview from "./cms-preview-templates/values";
-import ContactPreview from "./cms-preview-templates/contact";
-
-CMS.registerPreviewStyle(styles, { raw: true });
-CMS.registerPreviewTemplate("home", HomePreview);
-CMS.registerPreviewTemplate("post", PostPreview);
-CMS.registerPreviewTemplate("values", ValuesPreview);
-CMS.registerPreviewTemplate("contact", ContactPreview);
-
-// ----- end demo stuff -----
-
 import ArtworkPreview from "./cms-preview-templates/artwork";
 import AboutUsPreview from "./cms-preview-templates/about-us";
 import ProgramsPreview from "./cms-preview-templates/programs";
