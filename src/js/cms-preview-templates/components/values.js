f5325be0eb10ca3dbc1bc8988182453c2c3ad742
John Cipriano <johnmikecip@gmail.com>
2021-05-21 19:28:26 -0700

    move class to outside

--- src/js/cms-preview-templates/components/values.js
+++ src/js/cms-preview-templates/components/values.js
@@ -2,13 +2,14 @@ import React from "react"
 
 export default () =>
     <div>
-        <h1 className="sectionHeading">Artwork</h1>
         <p className="u-text-medium">As muralists, we seek to...</p>
-        <ul className="u-text-medium u-text-2xHeight">
-            <li>Use art to promote community engagement and development</li>
-            <li>Help communities express themselves and find/explore the power of the visual voice</li>
-            <li>Help share the unique story of each community</li>
-            <li>Develop neighborhood beautification projects and reclaim public spaces</li>
-            <li>Teach a high quality process of fine-art making in the creation of a community mural</li>
-        </ul>
+        <div class="u-text-medium u-text-2xHeight">
+            <ul>
+                <li>Use art to promote community engagement and development</li>
+                <li>Help communities express themselves and find/explore the power of the visual voice</li>
+                <li>Help share the unique story of each community</li>
+                <li>Develop neighborhood beautification projects and reclaim public spaces</li>
+                <li>Teach a high quality process of fine-art making in the creation of a community mural</li>
+            </ul>
+        </div>
     </div>
